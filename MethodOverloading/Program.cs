defbf0f888a5f07a84e6bb13fbb6789f57b50b48
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-09-09 19:17:09 +0530

    Add files via upload

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void printInfo(int id,string name)
+        {
+            Console.WriteLine("Roll No : " + id);
+            Console.WriteLine("Name : " + name);
+        }
+        static void printInfo(string name,int id)
+        {
+            Console.WriteLine($"Roll No : {id}");
+            Console.WriteLine($"Name : {name}");
+        }
+        static void printInfo(string name)
+        {
+            Console.WriteLine($"Name : {name}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Method:");
+            printInfo(518, "Thirumala Naidu");
+            Console.WriteLine("Second Method:");
+            printInfo("Thiurmala Naidu", 238);
+            Console.WriteLine("Third Method:");
+            printInfo("Thirumala Naidu");
+            Console.ReadLine();
+        }
+    }
+}
