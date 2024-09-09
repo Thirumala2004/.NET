47478f99f6605414f874a1827b7062cf4dc071d1
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-09-09 19:18:04 +0530

    Add files via upload

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Dog
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Dog is Eating");
+        }
+    }
+    public class Cat:Dog
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Cat is Eating");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c = new Cat();
+            c.eat();
+            Console.ReadLine();
+        }
+    }
+}
