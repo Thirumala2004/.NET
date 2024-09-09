4df3cfe3ba2358f23496d9d7dfd824bb73db1023
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-09-09 18:15:04 +0530

    Add files via upload

---
+++ Expt1/Expt1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 4;
+            Console.WriteLine("Addition of Two Numbers is {0}", a + b);
+            Console.WriteLine("Substraction of Two Numbers is {0}", a - b);
+            Console.WriteLine("Multiplication of Two Numbers is {0}", a * b);
+            Console.WriteLine("Division of Two Numbers is {0}", (float)a / b);
+            Console.Read();
+        }
+    }
+}
