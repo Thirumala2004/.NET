9c5001e0a9a0f7c7401d5983c5fb6a48ced7d9db
Thirumala2004 <thirumalanaiduponnaganti@gmail.com>
2024-09-09 18:17:24 +0530

    Add files via upload

---
+++ Expt2/Expt2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                Console.WriteLine(i);
+                sum = sum + i;
+            }
+            Console.WriteLine("The Sum of first 10 Natural Numbers is {0}",sum);
+            Console.Read();
+        }
+    }
+}
